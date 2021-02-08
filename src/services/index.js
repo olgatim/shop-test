2e7eb9fa59d9569703daea0f213ed26363fe9c87
olga_tim <tigercub220@gmail.com>
2021-02-08 03:57:25 +0200

    Update services

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,8 @@
+import {
+  getProducts as getDummyProducts,
+  getSingleProduct as getDummySingleProduct,
+} from "./dummyService";
+
 const apiBase = "http://167.172.186.154/api";
 
 const getResource = async (url) => {
@@ -10,14 +15,21 @@ const getResource = async (url) => {
   return res;
 };
 
-const getAllProducts = async () => {
+const getProducts = async () => {
   const res = await getResource("/products/");
   return res;
 };
 
-const getProduct = async (id) => {
+const getSingleProduct = async (id) => {
   const product = await getResource(`/products/${id}`);
   return product;
 };
+
+const getAllProducts =
+  process.env.NODE_ENV === "development" ? getDummyProducts : getProducts;
+const getProduct =
+  process.env.NODE_ENV === "development"
+    ? getDummySingleProduct
+    : getSingleProduct;
 
 export { getAllProducts, getProduct };
